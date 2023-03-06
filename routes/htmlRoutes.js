c174b82a6cc302ac99c362f41142b33ba355e473
El Elnimeiry <elsadekelnimeriy@gmail.com>
2023-03-06 16:01:36 -0600

    created server js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,4 @@
-const app = require("express").Routes()
+const app = require("express").Router()
 const path = require('path')
 
 
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require(“express”)
+const app = express()
+const PORT = process.env.PORT || 3003
+const apiRoutes = require(“./routes/apiRoutes”)
+const htmlRoutes = require(“./routes/htmlRoutes”)
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static(__dirname + “/public”));
+app.use(apiRoutes)
+app.use(htmlRoutes)
+app.listen(PORT, function(){
+    console.log(“Server started on port ” + PORT)
+})
